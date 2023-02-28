8fc0ab857f011652b3670bdbc9b9897ee4142b0c
sirotkin <sirotkin.nikola@mail.ru>
2023-02-28 14:20:39 +0300

    save

--- online_store/my_store_app/urls.py
+++ online_store/my_store_app/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import include, path
 from my_store_app.views import *
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', CategoryView.as_view(), name='index'),
     path('register/', register_view, name='register'),
     path('logout/', AuthorLogoutView.as_view(), name='logout'),
     path('login/', Login.as_view(template_name='login.html'), name='login')
-
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- online_store/online_store/settings.py
+++ online_store/online_store/settings.py
@@ -99,14 +99,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-
-
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 LOGIN_REDIRECT_URL = '/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+STATICFILES_DIRS = []
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
