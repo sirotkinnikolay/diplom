6b3574a7d141e79b3c8de6f4295c4b99ddeeda19
sirotkin <sirotkin.nikola@mail.ru>
2023-02-28 11:26:53 +0300

    save

--- online_store/my_store_app/urls.py
+++ online_store/my_store_app/urls.py
@@ -3,9 +3,9 @@ from django.urls import include, path
 from my_store_app.views import *
 
 urlpatterns = [
-    path('', CategoryView.as_view()),
+    path('', CategoryView.as_view(), name='index'),
     path('register/', register_view, name='register'),
-    path('login/', AuthorLoginView.as_view(), name='login'),
     path('logout/', AuthorLogoutView.as_view(), name='logout'),
+    path('login/', Login.as_view(template_name='login.html'), name='login')
 
 ]
--- online_store/my_store_app/views.py
+++ online_store/my_store_app/views.py
@@ -1,9 +1,13 @@
+from django.contrib import messages
 from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
 from django.views import View
 from my_store_app.models import *
 from my_store_app.forms import *
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.views import LogoutView, LoginView
+from django.contrib.auth.models import User
+
 
 # ====================регистрация и аутентификация =====================================================================
 
@@ -12,23 +16,18 @@ def register_view(request):  # +
     """Функция регистрации нового пользователя"""
 
     if request.method == 'POST':
-        print(request.POST)
         form = AuthorRegisterForm(request.POST)
         if form.is_valid():
-            print('форма валидна')
             full_name = form.cleaned_data.get('full_name')
             phone = form.cleaned_data.get('phone')
             email = form.cleaned_data.get('email')
             raw_password = form.cleaned_data.get('password')
             username = form.cleaned_data.get('login')
-            print(username)
-            print(raw_password)
-
-            # AttributeError: 'AnonymousUser' object has no attribute '_meta'
-
-            user = authenticate(username=username, password=raw_password)
+            user = User.objects.create_user(username=username, first_name=full_name, email=email)
+            user.set_password(raw_password)
+            user.save()
             login(request, user)
-            Profile.objects.create(username=username, full_name=full_name, phone=phone, user_id=user.id, email=email)
+            Profile.objects.create(user=user, username=username, full_name=full_name, phone=phone, email=email)
         return redirect('/')
 
     return render(request, 'registr.html')
@@ -39,13 +38,12 @@ class AuthorLogoutView(LogoutView):  # +
     next_page = '/'
 
 
-class AuthorLoginView(LoginView):  # +
+class Login(LoginView):
     """Вход в учетную запись"""
-    template_name = 'login.html'
-
+    def form_valid(self, form):
+        return super().form_valid(form)
 
 # ======================================================================================================================
-
 
 class CategoryView(View):
     def get(self, request):
